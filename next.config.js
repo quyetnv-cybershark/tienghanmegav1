4e690ed191dc612db0990cb8e6f003823ce31359
quyetnv <quyetpm2x@gmail.com>
2024-09-16 02:52:30 +0700

    feat: update load img

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: '/personal-portfolio',
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   basePath: '/personal-portfolio',
+// };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
