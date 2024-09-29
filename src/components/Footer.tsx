b8e423f76b4356f147755b0827cc7dee7650fe98
quyetnv <quyetpm2x@gmail.com>
2024-09-29 18:41:10 +0700

    feat: fb plugin

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -158,9 +158,10 @@ const Footer = () => {
             border: 'none',
             overflow: 'hidden',
           }}
+          title='123'
           scrolling='no'
-          frameborder='0'
-          allowfullscreen='true'
+          frameBorder='0'
+          allowFullScreen={true}
           allow='autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share'
         ></iframe>
       </div>
